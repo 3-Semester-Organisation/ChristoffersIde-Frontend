ce5bf2153f1a618a86e987a227bf061cc807e7de
madsfjeldberg <mafj0001@stud.kea.dk>
2024-11-04 23:19:46 +0100

    updated ui to use textfield

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,50 +1,49 @@
 import React, { useState } from "react";
-import { fetchRecipe } from "../api/recipeapi"
+import { Bars } from "react-loading-icons";
 
-function fetch(name) {
-    fetchRecipe(name).then((data) => {
-        console.log(data);
-    });
-}
+export default function Input({ handleFetch }) { //modtager handleFetch fra parent component (Dialog.jsx)
 
-export default function Input() {
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false); // used for loading icon on submit
 
-    const [name, setName] = useState("");
-    
-    const handleInputChange = (event) => {
-        setName(event.target.value);
-    };
+  const handleInputChange = (event) => {
+    setName(event.target.value); // opdaterer state af name ved hver change
+  };
 
-    const handleSubmit = () => {
-        fetch(name);
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      await handleFetch(name); // kører handleFetch modtaget fra parent component (Dialog.jsx)
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <>
-          <div className="relative mt-2 rounded-md shadow-sm flex justify-center">
+  return (
+    <>
+      <div className="relative mt-2 mb-10 rounded-md flex justify-center">
+        <form onSubmit={handleSubmit} className="flex items-center">
           <input
             id="name"
             name="name"
             type="text"
             placeholder="Name"
             value={name}
-            onChange={handleInputChange}
-            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+            onChange={handleInputChange} // sætter "name" state til indhold af input
+            className="w-full rounded-md border-0 pr-20 text-gray-900 ring-1 ring-inset ring-indigo-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+            style={{ height: "40px" }}
+            required
           />
           <button
-          type="button"
-          onClick={handleSubmit}
-          className=" cursor-pointer rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500"
-        >
-          Submit
-        </button>
-          </div>
-        
-      </>
-      
-      
-    )
-  }
-  
-
-  
+            type="submit"
+            className="ml-2 cursor-pointer rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500"
+            disabled={loading} // samme som nedenunder
+          >
+            {loading ? <Bars height="20px" width="46px" /> : "Submit"} {/* skifter mellem loading icon og submit alt efter state */}
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
