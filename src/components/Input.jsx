01d16dc91d32c9e7b6f2883dac0c4be899454ada
madsfjeldberg <mafj0001@stud.kea.dk>
2024-11-04 16:08:34 +0100

    fixed button

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,30 +1,27 @@
+import { fetchRecipe } from "../api/recipeapi"
+
 export default function Input() {
     return (
-        <form>
-        <div className="flex justify-center" >
-            <div className="relative mt-2 rounded-md shadow-sm">
-          <div className="pointer-events-none absolute flex items-center">
-          </div>
+        <>
+          <div className="relative mt-2 rounded-md shadow-sm flex justify-center">
           <input
-            id="price"
-            name="price"
+            id="name"
+            name="name"
             type="text"
             placeholder="Name"
             className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
           />
-        </div>
-        </div>
-        <div className="mt-6 flex items-center justify-center gap-x-6">
-        <button
-          type="submit"
-          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          style={{ transition: 'background-color 0.3s' }}
+          <button
+          type="button"
+          className=" cursor-pointer rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500"
         >
           Submit
         </button>
-        </div>
+          </div>
+        
+      </>
       
-      </form>
+      
     )
   }
   
