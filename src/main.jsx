6843eb6a16135a6ec51c85d9f3784a1ae141d25f
Chris <chye0001@stud.kea.dk>
2024-11-05 13:21:00 +0100

    added router to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import NotFoundPage from "./components/NotFoundPage.jsx";
+import IngredientSubmitter from "./components/IngredientSubmitter.jsx";
+import HomePage from "./components/HomePage.jsx";
+import Header from "./components/Header.jsx";
+import Dialog from "./components/Dialog.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage/>,
+        errorElement: <NotFoundPage/>
+    },
+    {
+        path: '/recipe-person-name',
+        element: <Dialog/>
+    },
+    {
+        path: '/recipe',
+        element: <IngredientSubmitter />,
+        errorElement: <NotFoundPage/>
+    }
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <Header/>
+        <RouterProvider router={router}/>
+    </StrictMode>,
 )
